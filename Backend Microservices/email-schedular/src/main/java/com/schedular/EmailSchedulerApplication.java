3f568ca1710828b8ca65fd62aded2a4c1ca760fc
Ankush Goyal <agoyal@advatix.com>
2021-09-25 15:54:45 +0530

    Updated Code

--- Backend Microservices/email-schedular/src/main/java/com/schedular/EmailSchedulerApplication.java
+++ Backend Microservices/email-schedular/src/main/java/com/schedular/EmailSchedulerApplication.java
@@ -1,67 +1,26 @@
 package com.schedular;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Properties;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.client.RestTemplate;
+
 import com.schedular.commons.Logger;
-import com.schedular.commons.StringUtils;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableAsync
-@EnableJpaRepositories
-@EnableSwagger2
-// @PropertySource(ignoreResourceNotFound = false,
-// value = {"classpath:db.properties", "classpath:mailconfig.properties"})
-@PropertySource(ignoreResourceNotFound = true,
-    value = {
-        "file:${" + EmailSchedulerApplication.PROPERTIES_LOCATION_ENV + "}/"
-            + EmailSchedulerApplication.APPLICATION_PROPERTY + ".yml",
-        "file:${" + EmailSchedulerApplication.PROPERTIES_LOCATION_ENV + "}/"
-            + EmailSchedulerApplication.DB_PROPERTY + ".properties",
-        "file:${" + EmailSchedulerApplication.PROPERTIES_LOCATION_ENV + "}/"
-            + EmailSchedulerApplication.MAIL_PROPERTY + ".properties",
-        "file:${" + EmailSchedulerApplication.PROPERTIES_LOCATION_ENV + "}/"
-            + EmailSchedulerApplication.ERROR_MESSAGES_PROPERTY + ".properties"})
 public class EmailSchedulerApplication {
 
-  private static Logger log = Logger.getLogger(EmailSchedulerApplication.class);
-
-  public static final String PROPERTIES_LOCATION_ENV = "spring.config.location";
-  public static final String APPLICATION_PROPERTY = "application";
-  public static final String DB_PROPERTY = "email-db";
-  public static final String MAIL_PROPERTY = "email-mailconfig";
-  public static final String ERROR_MESSAGES_PROPERTY = "email-error-messages";
-
-  protected static final List<String> PROPERTY_FILES =
-      Arrays.asList(APPLICATION_PROPERTY, DB_PROPERTY, MAIL_PROPERTY, ERROR_MESSAGES_PROPERTY);
-  public static final String PROPERTIES_FILE_NAME = String.join(",", PROPERTY_FILES);
-
-  public static void main(String[] args) {
-    String configLocation =
-        System.getProperty(EmailSchedulerApplication.PROPERTIES_LOCATION_ENV, "classpath:/");
-    String configPath = configLocation + " - " + EmailSchedulerApplication.PROPERTIES_FILE_NAME;
-    log.info("Configpath: {}", configPath);
-    if (StringUtils.isNotBlank(configLocation)) {
-      Properties props = new Properties();
-      props.setProperty(EmailSchedulerApplication.PROPERTIES_LOCATION_ENV, configLocation);
-      props.setProperty("spring.config.name", EmailSchedulerApplication.PROPERTIES_FILE_NAME);
-      ConfigurableApplicationContext applicationContext =
-          new SpringApplicationBuilder(EmailSchedulerApplication.class).properties(props).build()
-              .run(args);
-      applicationContext.getEnvironment();
-    } else {
-      SpringApplication.run(EmailSchedulerApplication.class, args);
-    }
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSchedulerApplication.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 }
